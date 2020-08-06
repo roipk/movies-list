ee7c0ac2ad5ce81da2a54cbf304343d333adaf6c
Roi Sulimani <roisulimani@gmail.com>
2020-08-06 05:35:28 +0300

    MovieDetails page desing adjustment

--- src/Components/movieDetails.js
+++ src/Components/movieDetails.js
@@ -3,10 +3,12 @@ import test from "./test.jpg";
 import { Container, Row, Col } from "react-bootstrap";
 import { Typography } from "@material-ui/core";
 
+// Class component which describes the details of a specific movie
 class MovieDetails extends React.Component {
   componentDidMount() {
     this.movie = this.props.location.state;
   }
+  // take the arguments from the props
   state = {
     movie: this.props.location.state,
   };
@@ -23,33 +25,25 @@ class MovieDetails extends React.Component {
                 </div>
               </Col>
               <Col xs={6} dir="rtl">
-                <div className="details">
+                <div className="details" style={{ textAlign: "right" }}>
                   <h1>{this.state.movie.movie.title}</h1>
                   <div className="subDetails">
-                    <span>{this.state.movie.movie.releaseDate} | </span>
-                    <span>{this.state.movie.movie.duration} דקות | </span>
-                    {this.state.movie.movie.genres.map((genre, idx) => {
-                      if (idx < this.state.movie.movie.genres.length - 1) {
-                        return <span>{genre}, </span>;
-                      } else {
-                        return <span>{genre} </span>;
-                      }
-                    })}
+                    <div style={{ color: "#eb8a90" }}>
+                      <span>{this.state.movie.movie.releaseDate} | </span>
+                      <span>{this.state.movie.movie.duration} דקות | </span>
+                      {this.state.movie.movie.genres.map((genre, idx) => {
+                        if (idx < this.state.movie.movie.genres.length - 1) {
+                          return <span>{genre}, </span>;
+                        } else {
+                          return <span>{genre} </span>;
+                        }
+                      })}
+                    </div>
                     <div>
                       <Typography variant="body1" gutterBottom>
                         {this.state.movie.movie.description}
                       </Typography>
                     </div>
-                    {/* <div>
-                      <span>Starring: </span>
-                      {this.state.movie.movie.cast.map((author, idx) => {
-                        if (idx !== test - 1) {
-                          return <span>{author}, </span>;
-                        } else {
-                          return <span>{author}</span>;
-                        }
-                      })}
-                    </div> */}
                   </div>
                 </div>
               </Col>
